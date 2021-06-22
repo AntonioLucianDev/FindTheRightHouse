b27e702c66171ecb44caf45d394eb9513d3cf0c3
Antonio Popa <web.antonio.popa@gmail.com>
2021-06-22 17:56:16 +0200

    Initial commit

--- src/main/java/com/asusoftware/FindTheRightHouse/user/repository/UserRepository.java
+++ src/main/java/com/asusoftware/FindTheRightHouse/user/repository/UserRepository.java
@@ -3,9 +3,14 @@ package com.asusoftware.FindTheRightHouse.user.repository;
 import com.asusoftware.FindTheRightHouse.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
+@Transactional(readOnly = true)
 public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByEmail(String email);
 }
--- src/main/java/com/asusoftware/FindTheRightHouse/user/service/UserService.java
+++ src/main/java/com/asusoftware/FindTheRightHouse/user/service/UserService.java
@@ -6,6 +6,9 @@ import com.asusoftware.FindTheRightHouse.user.model.dto.CreateUserDto;
 import com.asusoftware.FindTheRightHouse.user.model.dto.UserDto;
 import com.asusoftware.FindTheRightHouse.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,8 +18,9 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
-public class UserService {
+public class UserService implements UserDetailsService {
 
+    private final static String USER_NOT_FOUND_MSG = "User with email %s not found";
     private final UserRepository userRepository;
 
     public void create(CreateUserDto createUserDto) {
@@ -29,5 +33,10 @@ public class UserService {
 
     public User findUserById(UUID id) {
         return userRepository.findById(id).orElseThrow(UserNotFoundException::new);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException(String.format(USER_NOT_FOUND_MSG, email)));
     }
 }
